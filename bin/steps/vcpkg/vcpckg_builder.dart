d637054c383f7b4afeb5fb91404fd10f93932b65
Shigomany <ltropin@mail.ru>
2022-08-11 03:19:29 +0500

    Prepare to create macOS builder

--- bin/steps/vcpkg/vcpckg_builder.dart
+++ bin/steps/vcpkg/vcpckg_builder.dart
@@ -7,8 +7,39 @@ class VcpkgBuilder extends StepDefinitionExecutor {
   @override
   Future<void> step() => platform.when(
         windows: _winBuilder,
+        macOS: _macOsBuilder,
         orElse: () => throw UnsupportedError('Unsupported platfrom'),
       )!;
+
+  Future<void> _macOsBuilder() async {
+    if (!_isExistVcpkg()) {
+      await executeCommand(
+        command: 'bootstrap-vcpkg.bat',
+        rightPrompt: (done) => done
+            ? message('${'vcpkg'.bold} builded')
+            : message('${'vcpkg'.bold} installing'),
+        workingDirectory: 'vcpkg',
+      );
+    }
+    if (!_isInstalledProtobuf()) {
+      await executeCommand(
+        command: 'vcpkg install protobuf:x64-windows',
+        rightPrompt: (done) => done
+            ? message('${'protobuf'.bold} installed')
+            : message('${'protobuf'.bold} installing'),
+        workingDirectory: 'vcpkg',
+        runInShell: true,
+      );
+    }
+    await executeCommand(
+      command: 'vcpkg integrate install',
+      rightPrompt: (done) => done
+          ? message('${'vcpkg'.bold} integration installed')
+          : message('${'vcpkg'.bold} integration installing'),
+      workingDirectory: 'vcpkg',
+      runInShell: true,
+    );
+  }
 
   Future<void> _winBuilder() async {
     if (!_isExistVcpkg()) {
