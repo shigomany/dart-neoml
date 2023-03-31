ee66f536d4d63ce29eed8200505eebdce50d7654
Shigomany <ltropin@mail.ru>
2023-03-31 16:33:38 +0500

    Update dependecies

--- example/dart_neoml_example.dart
+++ example/dart_neoml_example.dart
@@ -4,9 +4,7 @@ void main() {
   final rnd = Random(seed: 42);
   print(rnd.uniform(min: 1, max: 100));
   print(rnd.uniform(min: 1, max: 100));
-  // SIGMOID в C++ передается хуево
   // final cls = LinearClassifier();
-  // print(ss2.sigmoid);
-  // print(ss.desc);
+
   // final rnd = Random();
 }
